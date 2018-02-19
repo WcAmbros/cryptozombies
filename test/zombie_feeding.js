8a4730c43e740d8ea4bf809d8357bc5f7d27d106
Zaharov <zaharov@softbalance.ru>
2018-02-19 20:07:59 +0300

    test

--- test/zombie_feeding.js
+++
@@ -1,9 +0,0 @@
-var ZombieFeeding = artifacts.require("./ZombieFeeding.sol");
-
-contract('ZombieFeeding', function(accounts) {
-  it("should assert true", function(done) {
-    var zombie_feeding = ZombieFeeding.deployed();
-    assert.isTrue(true);
-    done();
-  });
-});
